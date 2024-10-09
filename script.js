6abc5174f9722a78a34d554332d0d393b6fda03e
mirkiv <mirkamolmizraahmedov@gmail.com>
2024-10-09 16:14:12 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let counterBtnp = document.querySelector("#counterBtnp");
+let counterBtnpm = document.querySelector("#counterBtnm");
+let counterText = document.querySelector("#counterText");
+let counter = 0;
+counterBtnp.addEventListener("click", pFn);
+function pFn() {
+	counter++;
+	counterText.textContent = `Counter: ${counter}`;
+}
+counterBtnm.addEventListener("click", mFn);
+function mFn() {
+	counter--;
+	counterText.textContent = `Counter: ${counter}`;
+}
